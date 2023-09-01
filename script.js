3bc441ff58d3a74f295983b3e80d73bb3bdb29e9
Peter <skwortsow.petia@yandex.ru>
2023-09-01 17:59:53 +0300

    Добавил фоновое видео и немного фиксил

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const popUp = document.querySelector('.pop-up');
 const popUpImg = document.querySelector('.pop-up img');
 const popUpClose = document.querySelector('.pop-up span');
 const overlay = document.querySelector('.overlay');
+const card1 = document.querySelector('.card__o_nas-2'); 
 
 img.addEventListener('click', ()=> {
     popUp.style.display = 'block';
@@ -15,4 +16,22 @@ overlay.addEventListener('click', () => {
 popUpClose.addEventListener('click', () => {
     popUp.style.display = 'none';
 });
+
+//блок аккардеон
+
+const accordeon = document.querySelector('.feature-list');
+const accordeonBtns = accordeon.querySelectorAll('.feature__link');
+const accordeonText = accordeon.querySelectorAll('.feature-sub');
+
+accordeonBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        accordeonBtns.forEach((buttons) => {
+            buttons.classList.remove('feature__link_active');
+            buttons.nextElementSibling.classList.add('hidden')
+        })
+
+        btn.classList.toggle('feature__link_active');
+        btn.nextElementSibling.classList.toggle('hidden')
+    })
+})
 
